a7db322140941e4a2231363327323edaff613261
hiwina <hiwina@gmail.com>
2022-11-16 13:25:43 +0300

    fix error

--- menu.js
+++ menu.js
@@ -4,10 +4,9 @@ const navMenu = document.querySelector('.hidden-menu');
 humberger.addEventListener('click', () => {
   humberger.classList.toggle('active');
   navMenu.classList.toggle('active');
-});
+})
 
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-    humberger.classList.remove('active');
-    navMenu.classList.remove('active');
-}));
-  
+  humberger.classList.remove('active');
+  navMenu.classList.remove('active');
+}))
