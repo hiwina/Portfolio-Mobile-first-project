e7c21f218174aa5ad0f92a09d9583d6bf6d3971a
hiwina <hiwina@gmail.com>
2022-11-16 13:58:55 +0300

    Fix linter errors

--- menu.js
+++ menu.js
@@ -1,12 +1,13 @@
 const humberger = document.querySelector('.icon');
 const navMenu = document.querySelector('.hidden-menu');
+const cancel = document.querySelectorAll('.nav-link')
 
 humberger.addEventListener('click', () => {
   humberger.classList.toggle('active');
   navMenu.classList.toggle('active');
-})
+});
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+cancel.forEach(n => n.addEventListener('click', () => {
   humberger.classList.remove('active');
   navMenu.classList.remove('active');
-}))
+}));
