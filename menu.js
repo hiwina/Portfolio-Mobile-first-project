51160355f9e6535b943ab023a5da4ec9913b588e
hiwina <hiwina@gmail.com>
2022-11-17 12:13:02 +0300

    Add the HTML, css, javascript and linter files

---
+++ menu.js
@@ -0,0 +1,16 @@
+const humberger = document.querySelector('.icon');
+const navMenu = document.querySelector('.hidden-menu');
+const navLink = document.querySelectorAll('.nav-link');
+
+function openMenu() {
+  humberger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+function closeMenu() {
+  humberger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
+
+humberger.addEventListener('click', openMenu);
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
